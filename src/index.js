38eb6f629c4f4d121a4998919e4784691f5c02b4
Benno van den Berg <bennovandenberg@gmail.com>
2013-03-21 17:31:08 +0100

    Use openshift provided configuration

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000;
+var addr = process.env.OPENSHIFT_NODEJS_IP || process.env.ADDR || "localhost";
+
+app.listen(port, addr, function() {
   console.log("Listening on " + port);
 });
